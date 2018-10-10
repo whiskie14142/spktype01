964cc702daeb644ea15e76cfa0157d049493694d
whiskie14142 <shushi.uetsuki@nifty.com>
2018-10-11 08:33:56 +0900

    Version 1.0.0

--- package/spktype01/setup.py
+++ package/spktype01/setup.py
@@ -5,14 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spktype01",
+    license="MIT",
     version="1.0.0",
     author="whiskie14142",
     author_email="whiskie14142@gmail.com",
     description="A supporting module for jplephem to handle data type 1",
+    keywords="jplephem SPK type1",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/whiskie14142/spktype01",
-    packages=setuptools.find_packages(),
+    py_modules=["spktype01"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
